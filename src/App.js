aadd95bb114ff87b8a4ca2546c7275b0deac3e7a
Jean Koletto Reichmann GALY <hei.reichmann.4@gmail.com>
2023-02-01 01:13:56 +0300

    feat: add main page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
+function Todo(){
+    const [todoList,setTodoList] = useState([]);
+
+    const [toSave,setToSave] = useState({
+        id: '',
+        name: '',
+        isDone: false
+    })
+
+
+    const generateId = () => {
+        return (Date.now()
+            .toString(36) + Math.random().toString(36).slice(2, 7)).toUpperCase();
+    };
+
+    const handleChange = (event) => {
+        setToSave({
+            id: generateId(),
+            name: event.target.value,
+            isDone: false
+        });
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setTodoList([...todoList,toSave])
+        setToSave({
+            id: '',
+            name: '',
+            isDone: false
+        })
+        console.log(toSave)
+    }
+
+    return (
+        <>
+        <div className="container">
+            <div className="todo-container">
+                <h1>TODO</h1>
+                <form id="adding-form" onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        name="name"
+                        value={toSave.name}
+                        onChange={handleChange}/>
+                    <button type="submit" id="submit-button">Add</button>
+                </form>
+                <div className="todo">
+                    {todoList.filter((todo) => !todo.isDone).map((item, index)=>(
+                        <div key={index}>
+                            <span>{item.name}</span>
+                            <input id="status-checkbox" type="checkbox" onChange={() => {
+                                const updatedList = todoList.map(todo => {
+                                    if (todo.id === item.id) {
+                                        todo.isDone = true;
+                                    }
+                                    return todo;
+                                });
+                                setTodoList(updatedList);
+                            }}>
+                            </input>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className="done-container">
+                <h1>DONE</h1>
+                <div className="done">
+                    {todoList.filter((todo) => todo.isDone).map((item, index)=>(
+                        <div key={index}>
+                            <div key={index}>
+                                <span>{item.name}</span>
+                                <input id="status-checkbox" type="checkbox" defaultChecked="true" onChange={() => {
+                                    const updatedList = todoList.map(todo => {
+                                        if (todo.id === item.id) {
+                                            todo.isDone = false;
+                                        }
+                                        return todo;
+                                    });
+                                    setTodoList(updatedList);
+                                }}>
+                                </input>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Todo/>
     </div>
   );
 }
